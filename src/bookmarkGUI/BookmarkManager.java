1a87f49884e1dbef57f9f21886ce08968df98078
myeongjunkim <ilikerunning@naver.com>
2022-05-13 20:28:55 +0900

    bookmark layout

--- src/bookmarkGUI/BookmarkManager.java
+++ src/bookmarkGUI/BookmarkManager.java
@@ -1,10 +1,28 @@
 package bookmarkGUI;
 
+import java.awt.*;
 import javax.swing.*;
 
 
 public class BookmarkManager extends JFrame {
 	
-	
+	private static final long serialVersionUID = 1L;
+
+	public BookmarkManager() {
+		
+		JPanel p1 = new JPanel();
+		p1.setLayout(new GridLayout(5, 1));
+		p1.add(new JButton("ADD"));
+		p1.add(new JButton("DELETE"));
+		p1.add(new JButton("UP"));
+		p1.add(new JButton("DOWN"));
+		p1.add(new JButton("SAVE"));
+		
+//		전체 jframe
+		setLayout(new BorderLayout());
+		add(p1, BorderLayout.EAST);
+		add(new JButton("Food to be placed here"),
+			      BorderLayout.CENTER);
+	}
 
 }
---
+++ src/bookmarkGUI/TestGUI.java
@@ -0,0 +1,16 @@
+package bookmarkGUI;
+import javax.swing.*;
+
+public class TestGUI {
+
+	public static void main(String[] args) {
+		BookmarkManager frame = new BookmarkManager();
+	    frame.setTitle("BookmarkManager");
+	    frame.setSize(400, 250);
+	    frame.setLocationRelativeTo(null); // Center the frame
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setVisible(true);
+
+	}
+
+}
